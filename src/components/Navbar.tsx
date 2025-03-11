513a06192ff080c37fd4e77065c8628ba9d6ee30
CodeSplash7 <codesplash7@gmail.com>
2025-03-11 22:26:53 +0200

    refactor & style: responsivity, structure
    Make responsive styles.
    Structure code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,21 +10,19 @@ export default function Navbar() {
   const currentPath = usePathname();
 
   const links: NavLink[] = [
-    ["Home", "/"],
     ["Sign in", "/sign-in"],
-    ["Sign up", "/sign-up"]
   ];
 
   return (
     <div
-      className={`bg-[#111111] w-full h-[76px] px-[48px] flex items-center gap-[32px]`}
+      className={`bg-[#111111] w-full h-[76px] px-[48px] flex flex-row justify-between items-center gap-[32px] `}
     >
       {links.map((l, index) => (
         <Link
           key={index}
           className={`${
             isCurrentPath(l[1], currentPath) && "text-[#1cbac8]"
-          } hover:text-[#1cbac8] transition duration-150 text-[13px] text-white font-bold ${font_roboto_700}`}
+          } hover:text-[#1cbac8] transition duration-150 text-[24px] sm:text-[13px] text-white font-bold ${font_roboto_700}`}
           href={l[1]}
         >
           {l[0]}
--- src/page-components/home-page.tsx
+++ src/page-components/home-page.tsx
@@ -1,3 +1,4 @@
+import Br from "@/components/LineBreak";
 import Logo from "@/components/Logo";
 import Navbar from "@/components/Navbar";
 import Parallax from "@/components/Parallax";
@@ -5,21 +6,21 @@ import { font_roboto_500 } from "@/general-utils/fonts";
 
 export default async function HomePage() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-fit">
       <Navbar />
 
       <Parallax imagePath="/calisthenics-image.png">
         <div
-          className={`h-[calc(100vh-76px)] w-full px-[48px] bg-[rgba(0,0,0,0.5)]`}
+          className={`h-[calc(100vh-76px)] w-full px-[8px] sm:px-[48px] bg-[rgba(0,0,0,0.5)]`}
         >
-          <br />
-          <br />
-          <br />
+          <Br count={3} />
           <Logo />
-          <br />
+          <Br />
           <Motto msg="The best strength program ever." />
-          <br />
-          <br />
+          <Br count={2} />
+          <div className={"sm:hidden"}>
+            <Br count={6} />
+          </div>
           <StartButton />
         </div>
       </Parallax>
@@ -29,14 +30,18 @@ export default async function HomePage() {
 
 function Motto({ msg }: { msg: string }) {
   return (
-    <div className={`text-white text-[20px] ${font_roboto_500}`}>{msg}</div>
+    <div
+      className={`w-full text-white text-[24px] ${font_roboto_500} text-wrap`}
+    >
+      {msg}
+    </div>
   );
 }
 
 function StartButton() {
   return (
     <div
-      className={`px-[30px] w-fit py-[12.5px] border-0 rounded-full bg-[#1cbac8] text-white font-bold transition-all duration-200 hover:bg-[#ffbe5c] hover:shadow-[0_0_100px_#ffbe5c75] hover:scale-110 active:bg-[#1cbac8] active:duration-250 active:shadow-none active:scale-95`}
+      className={`sm:text-[1rem] text-[2rem] flex justify-center px-[30px] w-full sm:w-fit py-[20px] sm:py-[12.5px] border-0 rounded-full bg-[#1cbac8] text-white font-bold transition-all duration-200 hover:bg-[#ffbe5c] hover:shadow-[0_0_100px_#ffbe5c75] hover:scale-110 active:bg-[#1cbac8] active:duration-250 active:shadow-none active:scale-95`}
     >
       START 🗿
     </div>
