8670855725a3b7d3045310cee54b7d64a60a63b1
CodeSplash7 <codesplash7@gmail.com>
2025-03-11 22:10:54 +0200

    style: Finish mobile layout and style.
    Make the new page responsive for mobile devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,14 +17,14 @@ export default function Navbar() {
 
   return (
     <div
-      className={`bg-[#111111] w-full h-[76px] px-[48px] flex flex-row justify-between items-center gap-[32px] `}
+      className={`bg-[#111111] w-full h-[76px] px-[48px] flex items-center gap-[32px]`}
     >
       {links.map((l, index) => (
         <Link
           key={index}
           className={`${
             isCurrentPath(l[1], currentPath) && "text-[#1cbac8]"
-          } hover:text-[#1cbac8] transition duration-150 text-[24px] sm:text-[13px] text-white font-bold ${font_roboto_700}`}
+          } hover:text-[#1cbac8] transition duration-150 text-[13px] text-white font-bold ${font_roboto_700}`}
           href={l[1]}
         >
           {l[0]}
--- src/page-components/home-page.tsx
+++ src/page-components/home-page.tsx
@@ -6,12 +6,12 @@ import { font_roboto_500 } from "@/general-utils/fonts";
 
 export default async function HomePage() {
   return (
-    <div className="w-full h-fit">
+    <div className="w-full h-full">
       <Navbar />
 
       <Parallax imagePath="/calisthenics-image.png">
         <div
-          className={`h-[calc(100vh-76px)] w-full px-[8px] sm:px-[48px] bg-[rgba(0,0,0,0.5)]`}
+          className={`h-[calc(100vh-76px)] w-full px-[48px] bg-[rgba(0,0,0,0.5)]`}
         >
           <Br count={3} />
           <Logo />
@@ -30,18 +30,14 @@ export default async function HomePage() {
 
 function Motto({ msg }: { msg: string }) {
   return (
-    <div
-      className={`w-full text-white text-[24px] ${font_roboto_500} text-wrap`}
-    >
-      {msg}
-    </div>
+    <div className={`text-white text-[20px] ${font_roboto_500}`}>{msg}</div>
   );
 }
 
 function StartButton() {
   return (
     <div
-      className={`sm:text-[1rem] text-[2rem] flex justify-center px-[30px] w-full sm:w-fit py-[20px] sm:py-[12.5px] border-0 rounded-full bg-[#1cbac8] text-white font-bold transition-all duration-200 hover:bg-[#ffbe5c] hover:shadow-[0_0_100px_#ffbe5c75] hover:scale-110 active:bg-[#1cbac8] active:duration-250 active:shadow-none active:scale-95`}
+      className={`px-[30px] w-fit py-[12.5px] border-0 rounded-full bg-[#1cbac8] text-white font-bold transition-all duration-200 hover:bg-[#ffbe5c] hover:shadow-[0_0_100px_#ffbe5c75] hover:scale-110 active:bg-[#1cbac8] active:duration-250 active:shadow-none active:scale-95`}
     >
       START 🗿
     </div>
