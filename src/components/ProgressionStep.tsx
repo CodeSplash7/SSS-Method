7e4dc01b0af7f269929fc7f7a97c928b387f8b35
CodeSplash7 <codesplash7@gmail.com>
2025-03-13 18:24:13 +0200

    feat(ProgressionStep): improve component behavior and animations
    - set client side rendering
    - add transitioning animations

--- src/components/ProgressionStep.tsx
+++ src/components/ProgressionStep.tsx
@@ -1,22 +1,162 @@
-const ProgressionStep = () => (
-  <div className="z-30 w-screen h-screen bg-[#1cbac875] flex justify-center items-center">
-    <div className="w-[600px] scale-75 bg-white flex flex-col p-[48px] justify-start items-center gap-[32px] rounded-[14px]">
-      <div className="bg-red-500 text-white font-bold text-[20px] text-center rounded-full px-[32px] py-[12px]">
-        ONE MORE STEP!
-      </div>
-      <div className="flex flex-col text-[#6741d9] text-[40px] items-center">
-        <div>Progression Level From</div>
-        <div className="font-bold">REQUIRED</div>
-      </div>
-      <div className="w-2/3 text-center text-[18px]">
-        How can I give you the best training plan without knowing your level?
-      </div>
-      <br />
-      <div className="bg-[#1cbac8] text-white px-[48px] py-[24px] text-[30px] font-bold">
-        FILL FORM 🗿
-      </div>
-    </div>
-  </div>
-);
+"use client";
 
+import { animate } from "@/general-utils/app-routes";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const ProgressionStep = () => {
+  const [title, setTitle] = useState("⚠️ONE MORE STEP!⚠️");
+  const router = useRouter();
+  const transformBgBlock = async () => {
+    const previousBgElement = document.getElementById("previous-bg-element");
+    if (!previousBgElement) return;
+
+    const element = document.getElementById("button-to-form");
+    if (!element) return;
+    const rect = element.getBoundingClientRect();
+
+    animate(".child", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: { opacity: 1 },
+      toStyles: { opacity: 0 },
+    });
+
+    animate("#previous-bg-element", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: {
+        backgroundColor: "#ffffff",
+        // top: "7.5px",
+        borderRadius: "10.5px",
+        width: "375px",
+        height: "450px",
+        paddingTop: "36px",
+      },
+      toStyles: {
+        backgroundColor: "#111111",
+        // top: "10px",
+        width: "412.5px",
+        height: "225px",
+        paddingTop: "12px",
+      },
+    });
+
+    setTitle("Power Level Form");
+    await animate("#title", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: { color: "#1cbac8", opacity: 0 },
+      toStyles: { opacity: 1 },
+    });
+
+    animate("#transformer-text", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: { opacity: 1 },
+      toStyles: { opacity: 0 },
+    });
+
+    animate("#button-to-form", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: {
+        backgroundColor: "#1cbac8",
+        position: "absolute",
+        bottom: `36px`,
+      },
+      toStyles: {
+        backgroundColor: "white",
+        top: "75px",
+        position: "absolute",
+      },
+    });
+
+    await animate("#transformer-text", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: { display: "block" },
+      toStyles: { display: "none" },
+    });
+
+    await animate("#button-to-form", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: {
+        width: `262.5px`,
+      },
+      toStyles: {
+        width: "375px",
+      },
+    });
+
+    await animate("#button-to-form", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: {
+        height: `60px`,
+      },
+      toStyles: {
+        height: "450px",
+      },
+    });
+
+    await animate("#hidden-form", {
+      duration: 0.4,
+      easeFunc: "ease",
+      fromStyles: {
+        display: "none",
+        opacity: 0,
+      },
+      toStyles: { display: "flex", opacity: 1 },
+    });
+  };
+  return (
+    <div
+      id="previous-bg-element"
+      className="w-[375px] relative top-[10px] max-w-[90%] h-[450px] bg-white flex flex-col p-[36px] justify-between items-center rounded-[10.5px]"
+    >
+      <div
+        id="title"
+        className="child font-bold text-[22.5px] text-[#d30c7b] text-center rounded-full px-[24px] py-[9px]"
+      >
+        {title}
+      </div>
+
+      <div className="child text-center text-[#062f33] font-[1000] text-[34.5px]">
+        Power level form ⚡REQUIRED⚡
+      </div>
+
+      <div className="child w-full text-center text-[15px]">
+        How can I give you the best training plan without knowing your level?{" "}
+        <br />
+        🗿
+      </div>
+
+      <div
+        onClick={async () => {
+          await transformBgBlock();
+          router.push("power-level-form");
+        }}
+        id="button-to-form"
+        className="absolute z-[10] bottom-[36px] bg-[#1cbac8] w-[262.5px] h-[60px] flex items-center justify-center rounded-[15px] text-white text-[22.5px] font-bold"
+      >
+        <div id="transformer-text">FILL FORM 📋</div>
+        <div id="hidden-form" className="hidden opacity-0 text-black">
+          <div className="scale-150 dot-spinner">
+            <div className="dot-spinner__dot black"></div>
+            <div className="dot-spinner__dot black"></div>
+            <div className="dot-spinner__dot black"></div>
+            <div className="dot-spinner__dot black"></div>
+            <div className="dot-spinner__dot black"></div>
+            <div className="dot-spinner__dot black"></div>
+            <div className="dot-spinner__dot black"></div>
+            <div className="dot-spinner__dot black"></div>
+          </div>
+        </div>
+      </div>
+      <div className="child z-[1] bg-[#1cbac8] w-[262.5px] h-[60px] flex items-center justify-center rounded-[15px] text-white text-[22.5px] font-bold"></div>
+    </div>
+  );
+};
 export default ProgressionStep;
