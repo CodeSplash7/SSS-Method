cc539f0cf8891135bafd7d79480ffa101c5e9e1b
CodeSplash7 <codesplash7@gmail.com>
2025-03-14 19:20:08 +0200

    feat(QuestionPrompt): add new prop: isSameRoute

--- src/page-components/power-level-form-page.tsx
+++ src/page-components/power-level-form-page.tsx
@@ -5,133 +5,129 @@ import { Question } from "@/types/QuestionnaireTypes";
 import { cookies, headers } from "next/headers";
 
 export default async function PowerLevelFormPage({
-  searchParams,
-  params
+
+    searchParams,
+    params,
 }: {
-  params: Record<symbol, any>;
-  searchParams: { questionIndex: string };
+    params: Record<symbol, any>;
+    searchParams: { questionIndex: string };
 }) {
-  const currentRoute = params[Object.getOwnPropertySymbols(params)[7]].route;
-  const isSameRoute =
-    currentRoute === (await cookies()).get("previousPath")?.value;
+    const currentRoute = params[Object.getOwnPropertySymbols(params)[7]].route;
+    const isSameRoute =
+        currentRoute === (await cookies()).get("previousPath")?.value;
 
-  return (
-    <div className="z-30 w-full h-screen flex flex-col justify-start items-center">
-      <ScaleDownFormAnimation />
-      <div
-        className="w-[412.5px] h-[225px] relative top-[10px] bg-[#111111] max-w-[90%] flex flex-col p-[12px] justify-between items-center rounded-[10.5px]"
-        style={{
-          padding: `12px`
-        }}
-      >
-        {/* Form title*/}
-        <div className="font-bold text-[22.5px] text-[#1cbac8] text-center rounded-full px-[24px] py-[9px]">
-          Power Level Form
-        </div>
-
-        <div
-          id="form-card"
-          className="absolute z-[10] top-[75px] bg-white w-[375px] h-[450px] py-[18px] px-[36px] gap-[12px] flex flex-col items-center justify-start rounded-[15px] text-black text-[22.5px] font-bold"
-        >
-          {/* Progress Bar * */}
-
-          <div
-            id="progress-bar"
-            className="w-full h-[12px] bg-[#e7e5e5] rounded-[3px] text-[7.5px]"
-          >
+    return (
+        <div className="z-30 w-full h-screen flex flex-col justify-start items-center">
+            <ScaleDownFormAnimation />
             <div
-              id="progress-fill"
-              style={{
-                width: `${
-                  (Number(searchParams.questionIndex) + 1) *
-                  33.3333 *
-                  Number(isSameRoute)
-                }%`
-              }}
-              className="min-h-[3px] h-full bg-[#d30c7b] flex items-center justify-center text-white font-bold"
+                className="w-[412.5px] h-[225px] relative top-[10px] bg-[#111111] max-w-[90%] flex flex-col p-[12px] justify-between items-center rounded-[10.5px]"
+                style={{
+                    padding: `12px`,
+                }}
             >
-              {Number(searchParams.questionIndex) + 1}/3
+                {/* Form title*/}
+                <div className="font-bold text-[22.5px] text-[#1cbac8] text-center rounded-full px-[24px] py-[9px]">
+                    Power Level Form
+                </div>
+
+                <div
+                    id="form-card"
+                    className="absolute z-[10] top-[75px] bg-white w-[375px] h-[450px] py-[18px] px-[36px] gap-[12px] flex flex-col items-center justify-start rounded-[15px] text-black text-[22.5px] font-bold"
+                >
+                    {/* Progress Bar * */}
+                    <div
+                        id="progress-bar"
+                        className="w-full h-[12px] bg-[#e7e5e5] rounded-[3px] text-[7.5px]"
+                    >
+                        <div
+                            id="progress-fill"
+                            style={{
+                                width: `${(Number(searchParams.questionIndex) + 1) * 33.3333 * Number(isSameRoute)}%`,
+                            }}
+                            className="min-h-[3px] h-full bg-[#d30c7b] flex items-center justify-center text-white font-bold"
+                        >
+                            {Number(searchParams.questionIndex) + 1}/3
+                        </div>
+                    </div>
+                    <QuestionPrompt
+                        isSameRoute={isSameRoute}
+                        questionIndex={Number(searchParams.questionIndex)}
+                    />
+                </div>
             </div>
-          </div>
-          <QuestionPrompt
-            isSameRoute={isSameRoute}
-            questionIndex={Number(searchParams.questionIndex)}
-          />
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 function QuestionPrompt({
-  questionIndex,
-  isSameRoute
+    questionIndex,
+    isSameRoute,
 }: {
-  questionIndex: number;
-  isSameRoute: boolean;
+    questionIndex: number;
+    isSameRoute: boolean;
 }) {
-  const Questionnaire: Question[] = [
-    {
-      statement: "What is your level at pullups?",
-      options: [
-        { answer: "Beginner", detail: "0 pullups", emoji: "🔧" },
-        { answer: "Intermediate", detail: "1-15 pullups", emoji: "🔨" },
-        { answer: "Advanced", detail: "15+ pullups", emoji: "⚒️" },
+    const Questionnaire: Question[] = [
         {
-          answer: "Master",
-          detail: "(20kg+) x 8 Pullups",
-          emoji: "⚔️"
-        }
-      ]
-    },
-    {
-      statement: "What is your level at dips?",
-      options: [
-        { answer: "Beginner", detail: "0 dips", emoji: "🔧" },
-        {
-          answer: "Intermediate",
-          detail: "1-20m dips",
-          emoji: "🔨"
-        },
-        { answer: "Advanced", detail: "20+ dips", emoji: "⚒️" },
-        {
-          answer: "Master",
-          detail: "(40kg+) x 8 dips",
-          emoji: "⚔️"
-        }
-      ]
-    },
-    {
-      statement: "Choose your program for now",
-      options: [
-        {
-          answer: "4-day cycle",
-          detail: "Most efficient",
-          emoji: "⚡"
+            statement: "What is your level at pullups?",
+            options: [
+                { answer: "Beginner", detail: "0 pullups", emoji: "🔧" },
+                { answer: "Intermediate", detail: "1-15 pullups", emoji: "🔨" },
+                { answer: "Advanced", detail: "15+ pullups", emoji: "⚒️" },
+                {
+                    answer: "Master",
+                    detail: "(20kg+) x 8 Pullups",
+                    emoji: "⚔️",
+                },
+            ],
         },
         {
-          answer: "Weekly",
-          detail: "Most accesible",
-          emoji: "🔑"
-        }
-      ]
-    }
-  ];
-  const { statement, options } = Questionnaire[questionIndex];
-  return (
-    <>
-      <div
-        id="question"
-        className="text-[#343a40] text-[22.5px] h-fit text-center font-bold mt-[12px]"
-        style={{
-          fontSize: `22.5px`,
-          marginTop: `12px`,
-          opacity: `${Number(isSameRoute)}`
-        }}
-      >
-        {statement}
-      </div>
-      <QuestionnaireOptions options={options} isSameRoute={isSameRoute} />
-    </>
-  );
+            statement: "What is your level at dips?",
+            options: [
+                { answer: "Beginner", detail: "0 dips", emoji: "🔧" },
+                {
+                    answer: "Intermediate",
+                    detail: "1-20m dips",
+                    emoji: "🔨",
+                },
+                { answer: "Advanced", detail: "20+ dips", emoji: "⚒️" },
+                {
+                    answer: "Master",
+                    detail: "(40kg+) x 8 dips",
+                    emoji: "⚔️",
+                },
+            ],
+        },
+        {
+            statement: "Choose your program for now",
+            options: [
+                {
+                    answer: "4-day cycle",
+                    detail: "Most efficient",
+                    emoji: "⚡",
+                },
+                {
+                    answer: "Weekly",
+                    detail: "Most accesible",
+                    emoji: "🔑",
+                },
+            ],
+        },
+    ];
+    const { statement, options } = Questionnaire[questionIndex];
+    return (
+        <>
+            <div
+                id="question"
+                className="text-[#343a40] text-[22.5px] h-fit text-center font-bold mt-[12px]"
+                style={{
+                    fontSize: `22.5px`,
+                    marginTop: `12px`,
+                    opacity: `${Number(isSameRoute)}`,
+                }}
+            >
+                {statement}
+            </div>
+            <QuestionnaireOptions options={options} isSameRoute={isSameRoute} />
+        </>
+    );
 }
