c800c66d29c9b5593116905741f53b84b728f228
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 16:02:50 +0200

    feat: better animation handling
    - improve the scale animation handling of the form

--- src/components/ScaleFormOnScrollAnimationHandler.tsx
+++ src/components/ScaleFormOnScrollAnimationHandler.tsx
@@ -5,31 +5,47 @@ import {
     spreadOutFormAnimation,
 } from "@/animations/formScaleAnimation";
 import useUrl from "@/hooks/useUrl";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const ScaleFormOnScrollAnimationHandler = () => {
     const [URL] = useUrl();
     const [isCondensed, setIsCondensed] = useState(true);
 
     useEffect(() => {
-        if (isScaledDown === false) return;
-        scaleUpFormAnimation();
-        setIsScaledDown(false);
-    }, [URL]);
-    useEffect(() => {
         const options = document.getElementById("options");
         if (!options) return;
 
+        if (!isCondensed) {
+            condenseFormAnimation();
+            setIsCondensed(true);
+        }
+
         const handleScroll = () => {
-            scaleDownFormAnimation();
-            setIsScaledDown(true);
+            spreadOutFormAnimation();
+
+            setIsCondensed(false);
             options.removeEventListener("scroll", handleScroll);
         };
 
         options.addEventListener("scroll", handleScroll);
 
         return () => options.removeEventListener("scroll", handleScroll);
-    }, []);
+    }, [URL.queryParams.questionIndex]);
+    // useEffect(() => {
+    //     const options = document.getElementById("options");
+    //     if (!options) return;
+
+    //     const handleScroll = () => {
+    //         condenseFormAnimation();
+
+    //         setIsCondensed(true);
+    //         options.removeEventListener("scroll", handleScroll);
+    //     };
+
+    //     options.addEventListener("scroll", handleScroll);
+
+    //     return () => options.removeEventListener("scroll", handleScroll);
+    // }, []);
     return <></>;
 };
 export default ScaleFormOnScrollAnimationHandler;
