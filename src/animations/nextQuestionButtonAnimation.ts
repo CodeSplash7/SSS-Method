136c804f7d1f1ab2022ae64f753cf22ca6049592
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 14:44:05 +0200

    feat(animation): adjust "next" button animations
    - add one more animation per next question button animation action

--- src/animations/nextQuestionButtonAnimation.ts
+++ src/animations/nextQuestionButtonAnimation.ts
@@ -1,19 +1,31 @@
 import { animate } from "@/general-utils/app-routes";
 
 export const showNextQuestionButton = () => {
-    animate("#next-button", {
+    animate("#next-button-space-filler", {
         duration: 0.5,
         easeFunc: "ease",
         fromStyles: { height: "0px" },
         toStyles: { height: "60px" },
     });
+    animate("#next-button", {
+        duration: 0.5,
+        easeFunc: "ease",
+        fromStyles: { bottom: "-60px" },
+        toStyles: { bottom: "0px" },
+    });
 };
 
 export const hideNextQuestionButton = () => {
-    animate("#next-button", {
+    animate("#next-button-space-filler", {
         duration: 0.5,
         easeFunc: "ease",
         fromStyles: { height: "60px" },
         toStyles: { height: "0px" },
+    });
+    animate("#next-button", {
+        duration: 0.5,
+        easeFunc: "ease",
+        fromStyles: { bottom: "0px" },
+        toStyles: { bottom: "-60px" },
     });
 };
