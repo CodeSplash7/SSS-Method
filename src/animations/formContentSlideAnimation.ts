4402130ec2f77f14ec085d9b3a31bbadfa2c70c0
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 15:07:15 +0200

    feat(animate): conditional style
    - for the animations of the form sliding
    - use argument to/from, which return different animation results

--- src/animations/formContentSlideAnimation.ts
+++ src/animations/formContentSlideAnimation.ts
@@ -2,20 +2,22 @@ import { animate } from "@/general-utils/app-routes";
 
 const SLIDE_DURATION = 0.7;
 
-export const slideOut = async () => {
+export const slideOut = async (to: "left" | "right") => {
     await animate("#form-content", {
         duration: SLIDE_DURATION,
         easeFunc: "back.out",
         fromStyles: { transform: "translateX(0%)" },
-        toStyles: { transform: "translateX(-120%)" },
+        toStyles: { transform: `translateX(${to === "left" ? "-" : ""}120%)` },
     });
 };
 
-export const slideIn = async () => {
+export const slideIn = async (from: "left" | "right") => {
     await animate("#form-content", {
         duration: SLIDE_DURATION,
         easeFunc: "back.out",
-        fromStyles: { transform: "translateX(120%)" },
+        fromStyles: {
+            transform: `translateX(${from === "left" ? "-" : ""}120%)`,
+        },
         toStyles: { transform: "translateX(0%)" },
     });
 };
