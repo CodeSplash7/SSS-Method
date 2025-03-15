0948fde86b36fb346ea3b7c7972d768f09164bc3
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 15:20:46 +0200

    feat(questionnaireTypes): more props

--- src/types/QuestionnaireTypes.ts
+++ src/types/QuestionnaireTypes.ts
@@ -1,5 +1,12 @@
-export type QuestionOption = { answer: string; detail: string; emoji: string };
+export type QuestionOption = {
+    parentId: number;
+    id: number;
+    answer: string;
+    detail: string;
+    emoji: string;
+};
 export type Question = {
+    id: number;
     statement: string;
     options: QuestionOption[];
 };
