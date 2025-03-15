da48801dd452c449ee19be5a06964ee388b501cd
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 19:49:57 +0200

    feat: handle last question submission

--- src/components/NextQuestionButton.tsx
+++ src/components/NextQuestionButton.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/animations/nextQuestionButtonAnimation";
 // utils
 import replaceCharAtIndex from "@/general-utils/replaceCharAtIndex";
+import Questionnaire from "@/general-utils/Questionnaire";
+import delay from "@/general-utils/delay";
 
 export default function NextQuestionButton({
     selectedOption,
@@ -19,6 +21,19 @@ export default function NextQuestionButton({
 }) {
     const [URL, setURL] = useUrl();
     const [isNextButtonHidden, setIsNextButtonHidden] = useState<boolean>(true);
+    const [isLastQuestion, setIsLastQuestion] = useState<boolean>(
+        Number(URL.queryParams.questionIndex) + 1 === Questionnaire.length,
+    );
+
+    useEffect(() => {
+        (async () => {
+            await delay(500);
+            setIsLastQuestion(
+                Number(URL.queryParams.questionIndex) + 1 ===
+                    Questionnaire.length,
+            );
+        })();
+    }, [URL.queryParams.questionIndex]);
 
     useEffect(() => {
         const hasSelected = selectedOption.current !== null;
@@ -49,6 +64,16 @@ export default function NextQuestionButton({
         setURL(newURL);
     }
 
+    async function handleFinish() {
+        const newURL = { ...URL };
+        let { answers } = { ...newURL.queryParams };
+        answers += String(selectedOption.current);
+
+        newURL.queryParams = {};
+        newURL.route = "assessment";
+        setURL(newURL);
+    }
+
     return (
         <>
             <div
@@ -56,11 +81,11 @@ export default function NextQuestionButton({
                 className="opacity-0 w-full h-[0px] border-[1px] text-[20px]"
             ></div>
             <div
-                onClick={handleNext}
+                onClick={isLastQuestion ? handleFinish : handleNext}
                 id="next-button"
                 className="absolute -bottom-[60px] hover:bg-white hover:text-[#d30c7b] hover:border-[#d30c7b] transition duration-150 border-[1px] border-white text-[20px] bg-[#d30c7b] w-full h-[60px] overflow-hidden flex items-center justify-center text-white rounded"
             >
-                Next Question
+                {isLastQuestion ? "Finish" : "Next Question"}
             </div>
         </>
     );
