469a3671664d6bf892a762fab27655bdfe8a7746
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 15:14:34 +0200

    feat(utils): replace char function
    - in 'general-utils' directory

---
+++ src/general-utils/replaceCharAtIndex.ts
@@ -0,0 +1,16 @@
+export default function replaceCharAtIndex(
+  mainString: string,
+  index: number,
+  replacement: string,
+): string {
+  console.log(mainString, index, replacement);
+
+  if (index < 0 || index >= mainString.length) {
+    throw new Error("Index is out of bounds.");
+  }
+  if (replacement.length !== 1) {
+    throw new Error("Replacement must be a single character.");
+  }
+
+  return mainString.slice(0, index) + replacement + mainString.slice(index + 1);
+}
