acc0116a2d026c2d13dbe20d3de43b00cb40484b
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 11:53:24 +0200

    feat(component): form animation handler
    - component that handles the triggering of the animations of the form

---
+++ src/components/ScaleFormOnScrollAnimationHandler.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+    scaleDownFormAnimation,
+    scaleUpFormAnimation,
+} from "@/animations/formScaleAnimation";
+import useUrl from "@/hooks/useUrl";
+import { useEffect, useRef, useState } from "react";
+
+const ScaleDownFormOnScrollAnimation = () => {
+    const [URL] = useUrl();
+    const [isScaledDown, setIsScaledDown] = useState(false);
+
+    useEffect(() => {
+        if (isScaledDown === false) return;
+        scaleUpFormAnimation();
+        setIsScaledDown(false);
+    }, [URL]);
+    useEffect(() => {
+        const options = document.getElementById("options");
+        if (!options) return;
+
+        const handleScroll = () => {
+            scaleDownFormAnimation();
+            setIsScaledDown(true);
+            options.removeEventListener("scroll", handleScroll);
+        };
+
+        options.addEventListener("scroll", handleScroll);
+
+        return () => options.removeEventListener("scroll", handleScroll);
+    }, []);
+    return <></>;
+};
+export default ScaleDownFormOnScrollAnimation;
