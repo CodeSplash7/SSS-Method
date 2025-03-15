73d20db78e70e74e8414532cf194a4525b45938b
CodeSplash7 <codesplash7@gmail.com>
2025-03-15 15:44:02 +0200

    feat(previousQuestion): handle animations
    - implement the previousQuestionButton click effect animations and handle them using isAnimating state variable

--- src/components/PreviousQuestionButton.tsx
+++ src/components/PreviousQuestionButton.tsx
@@ -1,21 +1,57 @@
 "use client";
 
+import { animate } from "@/general-utils/app-routes";
+import delay from "@/general-utils/delay";
 import useUrl from "@/hooks/useUrl";
 import { useEffect, useState } from "react";
+
+async function handleShowAnimation() {
+    await delay(500); // duration for the form to slide out of view
+    animate("#previous-question-button", {
+        duration: 0.3,
+        easeFunc: "ease",
+        fromStyles: { opacity: 0 },
+        toStyles: { opacity: 1 },
+    });
+}
+
+async function handleHideAnimation() {
+    await delay(500); // duration for the form to slide out of view
+    animate("#previous-question-button", {
+        duration: 0.3,
+        easeFunc: "ease",
+        fromStyles: {},
+        toStyles: { opacity: 0 },
+    });
+}
 
 export default function PreviousQuestionButton() {
     // hooks
     const [URL, setURL] = useUrl();
     // state
-    const [isVisible, setIsVisisble] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+    const [isAnimating, setIsAnimating] = useState(false);
 
     useEffect(() => {
-        if (Number(URL.queryParams.questionIndex)) setIsVisisble(true);
-        else setIsVisisble(false);
+        if (Number(URL.queryParams.questionIndex)) setIsVisible(true);
+        else setIsVisible(false);
     }, [URL.toString()]);
 
+    useEffect(() => {
+        (async () => {
+            if (isAnimating) return;
+            setIsAnimating(true);
+            if (isVisible) await handleShowAnimation();
+            else await handleHideAnimation();
+
+            setIsAnimating(false);
+        })();
+    }, [isVisible]);
+
+    // NO IDEA what the problame here was but i count on future YOU
+
     function handleClick() {
-        if (!isVisible) return;
+        if (!isVisible || isAnimating) return;
         const newURL = { ...URL };
         newURL.queryParams.questionIndex = String(
             Number(newURL.queryParams.questionIndex) - 1,
@@ -25,12 +61,9 @@ export default function PreviousQuestionButton() {
 
     return (
         <div
+            id="previous-question-button"
             onClick={handleClick}
-            style={{
-                opacity: isVisible ? "1" : "0",
-                width: isVisible ? "fit-content" : "1px",
-            }}
-            className="relative w-fit h-fit p-[4px] bg-black rounded-[40%] top-[8px]"
+            className="opacity-0 w-[1px] w-[20px] flex items-center justify-center h-[20px] bg-black rounded-[40%]"
         >
             <svg
                 xmlns="http://www.w3.org/2000/svg"
